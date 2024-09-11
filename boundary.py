0e026378f3046f75131e7e8167ba0f52fb5b6a4e
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2024-09-11 17:13:38 +0200

    Renamed config files (and the experiment names) and the boundary condition names

--- boundary.py
+++ boundary.py
@@ -22,7 +22,7 @@ class Boundary:
             case 'top_left_fixed':
                 top_left_corner = torch.exp(-(curvilinear_coords[...,0:1] ** 2 + (curvilinear_coords[...,1:2] - self.curvilinear_space.xi__2_max) ** 2)/self.boundary_support)
                 deformations = deformations * (1 - top_left_corner) * initial_condition
-            case 'top_left_top_right_drape':
+            case 'top_left_top_right_moved':
                 top_left_corner = torch.exp(-(curvilinear_coords[...,0:1] ** 2 + (curvilinear_coords[...,1:2] - self.curvilinear_space.xi__1_max) ** 2)/0.01)
                 top_right_corner = torch.exp(-((curvilinear_coords[...,0:1] - self.curvilinear_space.xi__1_max) ** 2 + (curvilinear_coords[...,1:2] - self.curvilinear_space.xi__2_max) ** 2)/0.01)
                 #top_left_corner = torch.exp(-(curvilinear_coords[...,0:1] ** 2 + curvilinear_coords[...,1:2] ** 2)/0.01)
@@ -34,20 +34,20 @@ class Boundary:
             case 'adjacent_edges_fixed':
                 boundary_condition = curvilinear_coords[...,0][...,None] * curvilinear_coords[...,1][...,None]
                 output = output * boundary_condition * initial_condition
-            case 'center':
+            case 'nonboundary_handle_fixed':
                 center_point = torch.exp(-((curvilinear_coords[...,0:1] - 0.5 * self.curvilinear_space.xi__1_max) ** 2 + (curvilinear_coords[...,1:2] - 0.7 * self.curvilinear_space.xi__2_max) ** 2)/self.boundary_support)
                 deformations = deformations * (1 - center_point) * initial_condition
-            case 'center_edge':
+            case 'nonboundary_edge_fixed':
                 center_edge = torch.exp(-((curvilinear_coords[...,0:1] - 0.5 * self.curvilinear_space.xi__1_max) ** 2)/self.boundary_support)
                 deformations = deformations * (1 - center_edge)
-            case 'two_rims_compression':                
+            case 'top_bottom_rims_compression':                
                 bottom_rim = torch.exp(-(curvilinear_coords[...,1:2] ** 2)/self.boundary_support)
                 top_rim = torch.exp(-((curvilinear_coords[...,1:2] - self.curvilinear_space.xi__2_max) ** 2)/self.boundary_support)
                 #rim_displacement = torch.cat([torch.zeros_like(temporal_coords), 0.075 * temporal_coords, torch.zeros_like(temporal_coords)], dim=2)
                 rim_displacement = torch.cat([torch.zeros_like(temporal_coords), 0.075 * torch.ones_like(temporal_coords), torch.zeros_like(temporal_coords)], dim=2)
                 #deformations = deformations * (1 - bottom_rim) * (1 - top_rim) * initial_condition - rim_displacement * top_rim + rim_displacement * bottom_rim
                 deformations = deformations * (1 - bottom_rim) * (1 - top_rim) - rim_displacement * top_rim + rim_displacement * bottom_rim
-            case 'two_rims_torsion':
+            case 'top_bottom_rims_torsion':
                 bottom_rim = torch.exp(-(curvilinear_coords[...,1][...,None] ** 2)/0.01)
                 top_rim = torch.exp(-((curvilinear_coords[...,1][...,None] - self.theta__2_scale) ** 2)/0.01)
                 R = 0.25
