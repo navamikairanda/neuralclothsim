27685e427918fefa4ec38feec34aaa777e3ec779
Navami Kairanda <nkairand@mpi-inf.mpg.de>
2024-09-11 10:33:39 +0200

    Updated default config values

--- config_parser.py
+++ config_parser.py
@@ -10,16 +10,16 @@ def get_config_parser():
     parser.add_argument('-n', '--expt_name', type=str, required=True, help='experiment name; also the name of subdirectory in logging_dir')
 
     # simulation parameters
-    parser.add_argument('--reference_geometry_name', type=str, required=True, help='name of the reference geometry; can be rectangle_xy, rectangle_xz,  cylinder, cone or mesh')
+    parser.add_argument('--reference_geometry_name', type=str, default='rectangle_xy', help='name of the reference geometry; can be rectangle_xy, rectangle_xz,  cylinder, cone or mesh')
     parser.add_argument('--xi__1_max', type=float, default=2 * math.pi, help='max value for xi__1; 2 * pi for cylinder or cone, and Lx for rectangle. min value for xi__1 is assumed to be 0')
     parser.add_argument('--xi__2_max', type=float, default=1, help='max value for xi__2; Ly for all reference geometries. min value for xi__2 is assumed to be 0')
-    parser.add_argument('--boundary_condition_name', type=str, required=True, help='name of the spatio-temporal boundary condition; can be top_left_fixed, top_left_top_right_drape for rectangle, and two_rims_compression for cylinder, and top_rim_fixed, top_rim_torsion for cone')
+    parser.add_argument('--boundary_condition_name', type=str, default='top_left_fixed', help='name of the spatio-temporal boundary condition; can be top_left_fixed, top_left_top_right_drape for rectangle, and two_rims_compression for cylinder, and top_rim_fixed, top_rim_torsion for cone')
     parser.add_argument('--gravity_acceleration', type=float, nargs='+', default=[0,-9.8, 0], help='acceleration due to gravity')
     
     # additional parameters if the reference geometry is a mesh
-    parser.add_argument('--reference_geometry_source', type=str, default='assets/meshes/flag.obj', help='source file for reference geometry')
-    parser.add_argument('--reference_mlp_n_iterations', type=int, default=5000, help='number of iterations for fitting the reference geometry MLP')
-    parser.add_argument('--reference_mlp_lrate', type=float, default=1e-5, help='learning rate for the reference geometry MLP')
+    parser.add_argument('--reference_geometry_source', type=str, default='assets/textured_uniform_1_020.obj', help='source file for reference geometry')
+    parser.add_argument('--reference_mlp_n_iterations', type=int, default=3000, help='number of iterations for fitting the reference geometry MLP')
+    parser.add_argument('--reference_mlp_lrate', type=float, default=5e-6, help='learning rate for the reference geometry MLP')
     parser.add_argument('--reference_boundary_vertices', type=int, nargs='+', help='vertices for boundary condition on the reference geometry')
     
     # material parameters
@@ -31,7 +31,7 @@ def get_config_parser():
     parser.add_argument('--thickness', type=float, default=0.0012, help='thickness or tau in meters')
     
     # material parameters for linear material
-    parser.add_argument('--youngs_modulus', type=float, default=9000, help='Young\'s modulus')
+    parser.add_argument('--youngs_modulus', type=float, default=5000, help='Young\'s modulus')
     parser.add_argument('--poissons_ratio', type=float, default=0.25, help='Poisson\'s ratio')
 
     # material parameters for nonlinear material
@@ -57,24 +57,23 @@ def get_config_parser():
     parser.add_argument('--E_12_max', type=float, help='E_12_max')
     
     # training options
-    parser.add_argument('--train_n_spatial_samples', type=int, default=100, help='N_omega, number of samples used for training; when reference geometry is an analytical surface, number of spatial grid samples along each curvilinear coordinate is square_root(N_omega)')
-    parser.add_argument('--train_n_temporal_samples', type=int, default=20, help='N_t, number of temporal samples, at training')
-    parser.add_argument('--siren_omega_0', type=float, default=30, help='omega_0 for SIREN')
-    parser.add_argument('--lrate', type=float, default=1e-5, help='learning rate')
-    parser.add_argument('--decay_lrate', action='store_true', help='whether to decay learning rate')
-    parser.add_argument('--lrate_decay_steps', type=int, default=10000, help='learning rate decay steps')
+    parser.add_argument('--train_n_spatial_samples', type=int, default=400, help='N_omega, number of samples used for training; when reference geometry is an analytical surface, number of spatial grid samples along each curvilinear coordinate is square_root(N_omega)')
+    parser.add_argument('--train_n_temporal_samples', type=int, default=10, help='N_t, number of temporal samples, at training')
+    parser.add_argument('--lrate', type=float, default=5e-6, help='learning rate')
+    parser.add_argument('--decay_lrate', action='store_true', default=True, help='whether to decay learning rate')
+    parser.add_argument('--lrate_decay_steps', type=int, default=5000, help='learning rate decay steps')
     parser.add_argument('--lrate_decay_rate', type=float, default=0.1, help='learning rate decay rate')    
-    parser.add_argument('--n_iterations', type=int, default=10000, help='total number of training iterations')
+    parser.add_argument('--n_iterations', type=int, default=5001, help='total number of training iterations')
     
     # logging/saving options
     parser.add_argument('--logging_dir', type=str, default='logs', help='root directory for logging')
-    parser.add_argument("--i_weights", type=int, default=200, help='frequency of saving NDF weights as checkpoints')
+    parser.add_argument("--i_weights", type=int, default=400, help='frequency of saving NDF weights as checkpoints')
     parser.add_argument("--i_summary", type=int, default=100, help='frequency of logging losses')
     parser.add_argument("--i_test", type=int, default=100, help='frequency of evaluating NDF and saving resulting meshes during training')
     
     # debug options
-    parser.add_argument('--debug', action='store_true', help='whether to run NeuralClothSim in debug mode; this will log the reference geometric quantities (e.g. metric and curvature tensor), the strains, and the simulated states to TensorBoard')
-    parser.add_argument('--i_debug', type=int, default=100, help='frequency of Tensordboard logging of debug info')
+    parser.add_argument('--debug', action='store_true', default=True, help='whether to run NeuralClothSim in debug mode; this will log the reference geometric quantities (e.g. metric and curvature tensor), the strains, and the simulated states to TensorBoard')
+    parser.add_argument('--i_debug', type=int, default=200, help='frequency of Tensordboard logging of debug info')
     
     # reload options
     parser.add_argument('--no_reload', action='store_true', help='do not resume training from checkpoint, rather train from scratch')
@@ -83,6 +82,6 @@ def get_config_parser():
     
     # testing options
     parser.add_argument('--test_n_spatial_samples', type=int, default=400, help='N_omega, the number of samples used for evaluation when reference geometry is an analytical surface; if the reference geometry is instead a mesh, this argument is ignored, and the samples (vertices and faces) used for evaluation will match that of template mesh') 
-    parser.add_argument('--test_n_temporal_samples', type=int, default=20, help='N_t, number of temporal samples, at training')
+    parser.add_argument('--test_n_temporal_samples', type=int, default=2, help='N_t, number of temporal samples, at training')
                             
     return parser
